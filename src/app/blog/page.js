b2c19ce4966f6871805f76b523b6ec6dc5227b7e
quirkexplorer <quirkyexplorer@gmail.com>
2024-07-10 15:48:54 -0700

    updated prod domain fetching

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,18 +1,19 @@
 //'use client'
 
 import React from 'react'
+import getDomain from '../lib/getDomain'
 
 async function getData() {
   // 1 endpoint
 
-  // const endpoint = "http://localhost:3000/api/posts"
-  // const res = await fetch(endpoint)
+  const endpoint = `${domain}/api/posts`
+  const res = await fetch(endpoint)
 
-  // if(!res.ok) {
-  //   throw new Error("Failed to fetch data")
-  // }
+  if(!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
 
-  // return res.json()
+  return res.json()
   return {items:[]}
 }
 
