1ffa027905c1844790e05c6d0fbd6e776ad6940a
quirkexplorer <quirkyexplorer@gmail.com>
2024-07-10 14:10:12 -0700

    'first commit'

---
+++ src/app/blog/[slug]/page.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function BlogPostDetail(params) {
+  console.log(params)
+
+  return (
+    <div>Hello detail view</div>
+  )
+}
---
+++ src/app/error/page.js
@@ -0,0 +1,30 @@
+//'use client'
+
+import React from 'react'
+
+async function getData() {
+  // 1 endpoint
+
+  const endpoint = "http://localhost:3000/pi/posts"
+  const res = await fetch(endpoint)
+
+  if(!res.ok) {
+    throw new Error("Failed to fetch data")
+  }
+
+  return res.json()
+}
+
+export default async function BlogPage() {
+  const data = await getData()
+  const items = data && data.items ? [...data.items] : []
+  return (
+    <main>
+
+      <div>page</div>
+      {items && items.map((item, i) => {
+        return <li key={`post-${i}`}>{item.title}</li>
+      })}
+    </main>    
+  )
+}
