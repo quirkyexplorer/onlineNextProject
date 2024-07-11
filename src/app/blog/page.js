81c4bddc1e538310fe1e34545c7128f2bb85511f
quirkexplorer <quirkyexplorer@gmail.com>
2024-07-11 14:42:21 -0700

    updated with console logs

--- src/app/blog/page.js
+++ src/app/blog/page.js
@@ -1,5 +1,3 @@
-
-
 import React from 'react'
 import getDomain from '../lib/getDomain'
 
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,13 @@
 import Image from "next/image";
 
 export default function Home() {
+
+  if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'development') {
+    console.log('Running in development mode');
+  } else if (process.env.NEXT_PUBLIC_VERCEL_ENV === 'production') {
+    console.log('Running in production mode');
+  }
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
