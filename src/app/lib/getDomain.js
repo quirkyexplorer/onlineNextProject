dbefaabaaa29c415209ed25b8262556623b5456a
quirkexplorer <quirkyexplorer@gmail.com>
2024-07-11 21:00:40 -0700

    extra space

--- src/app/lib/getDomain.js
+++ src/app/lib/getDomain.js
@@ -1,5 +1,7 @@
 export default function getDomain() {
+
   const protocol = process.env.NEXT_PUBLIC_VERCEL_ENV === "production" ? "https" : "http"
+  
   const domain = process.env.PUBLIC_URL ? process.env.PUBLIC_URL : "localhost:3000"
   return `${protocol}://${domain}`
 }
