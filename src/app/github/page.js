5e20afe4d10c27fffb3702a11a6b7f366e460a03
quirkexplorer <quirkyexplorer@gmail.com>
2024-07-15 14:00:13 -0700

    added swr for client fetching

---
+++ src/app/github/page.js
@@ -0,0 +1,26 @@
+'use client'
+import useSWR
+ from "swr"
+
+const fetcher = (url) => fetch(url).then((res) => res.json())
+
+export default function GithubProfile() {
+  const myGithubRepoProfile = "https://api.github.com/repos/quirkyexplorer/onlineNextProject"
+  const {data, error, isLoading} = useSWR(myGithubRepoProfile, fetcher)
+  if (error) return "An error happened"
+  if (isLoading) return "Loading..."
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <div className='flex gap-4'>
+      <strong> 👁️ subscribers {data.subscribers_count}</strong>{" "}
+
+      <strong> ❤️ star gazers {data.stargazers_count}</strong>{" "}
+
+      <strong> 💪 forks {data.forks_count}</strong>{" "}
+      </div>
+      
+
+    </div>
+  );
+}
